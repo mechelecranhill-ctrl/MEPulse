1176b5b1577f01aa83f5d3b8c59543b870776e59
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-02-27 11:55:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,4 @@
-const cacheName = 'me-connect-v1';
-const assets = ['./', './index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', function(event) {
+    // Ini cuma enjin kosong untuk melepasi syarat PWA Android
+    // Anda boleh tambah fungsi offline caching di sini nanti
 });
