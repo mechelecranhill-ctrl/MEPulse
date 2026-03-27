603cce7f949c0c990e567b78987245b4aca6127b
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 15:43:41 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,8 @@
-<script src="js/config.js"></script>
-<script src="js/api.js"></script>
-<script src="js/state.js"></script>
+const SB_URL = 'https://ywmsvowroxzhrjwrhsru.supabase.co';
+const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3bXN2b3dyb3h6aHJqd3Joc3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDU5MzcsImV4cCI6MjA4Nzg4MTkzN30.OHJ-I_T3QID8y8eaoOBWeG2nKd2FhHfzG4P515Rzfks';
 
-<script src="js/ui/table.js"></script>
-<script src="js/ui/mobile.js"></script>
-<script src="js/ui/district.js"></script>
-<script src="js/ui/template.js"></script>
-
-<script src="js/blueform/calculator.js"></script>
-<script src="js/blueform/generator.js"></script>
-<script src="js/blueform/print.js"></script>
-
-<script src="js/main.js"></script>
+const headers = {
+    apikey: SB_KEY,
+    Authorization: `Bearer ${SB_KEY}`,
+    'Content-Type': 'application/json'
+};
