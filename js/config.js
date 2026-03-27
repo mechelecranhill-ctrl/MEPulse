2b4bcf952d2ec4195de1f94bb28e61bfb42a0c4b
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 14:40:09 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const SB_URL = 'https://ywmsvowroxzhrjwrhsru.supabase.co';
+export const SB_KEY = 'YOUR_KEY';
+
+export const headers = {
+  apikey: SB_KEY,
+  Authorization: `Bearer ${SB_KEY}`,
+  'Content-Type': 'application/json'
+};
