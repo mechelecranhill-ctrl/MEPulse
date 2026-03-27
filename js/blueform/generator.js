a2017527b22dfaa960e7ec5a23786badedbd80a4
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 16:49:55 +0800

    Update generator.js

--- js/blueform/generator.js
+++ js/blueform/generator.js
@@ -1,4 +1,4 @@
-import { state } from './state.js';
+ import { state } from './state.js';
 
 export function generateBlueform(contract, works){
     const container = document.getElementById('bf_content');
