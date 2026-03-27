c88ed2a2b8eee515bdf278eadf540ec8c987deeb
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 14:41:02 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const SB_URL = 'https://ywmsvowroxzhrjwrhsru.supabase.co';
-export const SB_KEY = 'YOUR_KEY';
+export const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3bXN2b3dyb3h6aHJqd3Joc3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDU5MzcsImV4cCI6MjA4Nzg4MTkzN30.OHJ-I_T3QID8y8eaoOBWeG2nKd2FhHfzG4P515Rzfks';
 
 export const headers = {
   apikey: SB_KEY,
