a3abd83dd7bc596511d3ffb350a5152757c1c85d
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 16:04:02 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,14 +1,26 @@
-async function fetchContract(id){
+// api.js
+import { SB_URL, headers } from './config.js';
+
+export async function fetchContract(id){
     const res = await fetch(`${SB_URL}/rest/v1/contract?id=eq.${id}`, { headers });
     return (await res.json())[0];
 }
 
-async function fetchWorkOrders(id){
+export async function fetchWorkOrders(id){
     const res = await fetch(`${SB_URL}/rest/v1/work_orders?contract_id=eq.${id}`, { headers });
     return await res.json();
 }
 
-async function fetchInterims(id){
+export async function fetchInterims(id){
     const res = await fetch(`${SB_URL}/rest/v1/interims?contract_id=eq.${id}`, { headers });
+    return await res.json();
+}
+
+export async function patchContract(id, payload){
+    const res = await fetch(`${SB_URL}/rest/v1/contract?id=eq.${id}`, {
+        method: 'PATCH',
+        headers,
+        body: JSON.stringify(payload)
+    });
     return await res.json();
 }
