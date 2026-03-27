2f16926f89b7483cf874bf80f57e7ec79c68a15a
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 15:42:44 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+<script src="js/config.js"></script>
+<script src="js/api.js"></script>
+<script src="js/state.js"></script>
+
+<script src="js/ui/table.js"></script>
+<script src="js/ui/mobile.js"></script>
+<script src="js/ui/district.js"></script>
+<script src="js/ui/template.js"></script>
+
+<script src="js/blueform/calculator.js"></script>
+<script src="js/blueform/generator.js"></script>
+<script src="js/blueform/print.js"></script>
+
+<script src="js/main.js"></script>
