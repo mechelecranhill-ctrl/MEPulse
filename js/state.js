6a5c15fe0116c58fc5f04e5a7c13f27d9bb50cad
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 15:20:38 +0800

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,8 @@
+export let currentView = "AREA";
+export let allWorks = [];
+export let currentContractId = null;
+export let areaPriority = {};
+
+export function setWorks(data){
+  allWorks = data;
+}
