7e24eb55fde15de2e3705a698c2aa730d5f92479
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 16:47:07 +0800

    Update calculator.js

--- js/blueform/calculator.js
+++ js/blueform/calculator.js
@@ -1,4 +1,3 @@
-// calculator.js
 export function calculateTotals(works){
     const totalWork = works.reduce((acc,w)=> acc + (w.amount_work || 0), 0);
     const totalClaim = works.reduce((acc,w)=> acc + (w.amount_claim || 0), 0);
