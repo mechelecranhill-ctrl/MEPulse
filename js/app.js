461b3619ffe22a98815890ad7f3528a66ccf42dc
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-03-27 15:01:51 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// app.js — entry point
+// 🔹 Import semua modul
+import './utils.js';
+import './api.js';
+import './config.js';
+import './state.js';
+import './table.js';
+import './template.js';
+import './blueform.js';
+import './main.js'; // pastikan main.js last, kalau ia panggil fungsi dari modul lain
