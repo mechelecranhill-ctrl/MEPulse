2d6be371ec8c4a73cce8fbc84e6917cfc4094c59
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-04-07 11:17:07 +0800

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const SB_URL = 'https://ywmsvowroxzhrjwrhsru.supabase.co'; 
-const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3bXN2b3dyb3h6aHJqd3Joc3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMDU5MzcsImV4cCI6MjA4Nzg4MTkzN30.OHJ-I_T3QID8y8eaoOBWeG2nKd2FhHfzG4P515Rzfks';
-
-const headers = {
-    apikey: SB_KEY,
-    Authorization: `Bearer ${SB_KEY}`,
-    'Content-Type': 'application/json'
-};
