ba164a8f777c010b9b3a39602ec6f5ddcf341240
mechelecranhill-ctrl <mech.elec.ranhill@gmail.com>
2026-07-05 13:44:49 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -41,22 +41,24 @@ class AppSidebar extends HTMLElement {
                 <a href="#" id="logoutBtn" class="menu-link">Logout</a>
             </div>
 
-            <button class="hamburger" id="hamburger">
+<div class="hamburger-wrapper">
     <div class="hamburger-box"></div> 
-    
-    <input type="checkbox" id="menuToggle">
-    <svg viewBox="0 0 32 32">
-        <path class="line line-top-bottom"
-            d="M27 10 13 10C10.8 10 9 8.2 9 6
-            9 3.5 10.8 2 13 2
-            15.2 2 17 3.8 17 6
-            L17 26C17 28.2 18.8 30 21 30
-            23.2 30 25 28.2 25 26
-            25 23.8 23.2 22 21 22
-            L7 22" />
-        <path class="line" d="M7 16 27 16" />
-    </svg>
-</button>
+
+    <button class="hamburger" id="hamburger">
+        <input type="checkbox" id="menuToggle">
+        <svg viewBox="0 0 32 32">
+            <path class="line line-top-bottom"
+                d="M27 10 13 10C10.8 10 9 8.2 9 6
+                9 3.5 10.8 2 13 2
+                15.2 2 17 3.8 17 6
+                L17 26C17 28.2 18.8 30 21 30
+                23.2 30 25 28.2 25 26
+                25 23.8 23.2 22 21 22
+                L7 22" />
+            <path class="line" d="M7 16 27 16" />
+        </svg>
+    </button>
+</div>
 
             <div class="logout-modal" id="logoutModal">
                 <div class="logout-box">
